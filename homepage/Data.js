4a440b75074fbcd779883725d4a532c75cd28b38
Shubham <sv7836832607@gmail.com>
2022-04-01 11:05:07 +0530

    price is in Number mow

--- homepage/Data.js
+++ homepage/Data.js
@@ -4,7 +4,7 @@ var slide1Obj = [{
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy9ubmJlYXJkby1tZWdhLWhlbXAtY29tYm8tNTEyeDUxMi0zNjg1MC5wbmciLCJlZGl0cyI6eyJyZXNpemUiOnsiZml0Ijoib3V0c2lkZSJ9fX0=",
         name: "Beardo Mega Hemp Combo",
         cutPrice: "₹ 1791",
-        price: "₹ 499",
+        price: "499",
         discountSticker: "https://cdn.beardo.in/vipclubsale2021/badge/VIP_Sale_Discount_Icon_72%25.png"
     },
     {
@@ -13,7 +13,7 @@ var slide1Obj = [{
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy93aXNoa3ktc21va2UtZGFyay1zaWRlLWRvbi1kZW8tbWFmaWEtZGVvLTUxMng1MTItMzczMTMucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7ImZpdCI6Im91dHNpZGUifX19",
         name: "Beardo AfterHours Perfume Combo",
         cutPrice: "₹ 3298",
-        price: "₹ 999",
+        price: "999",
         discountSticker: "https://cdn.beardo.in/vipclubsale2021/badge/VIP_Sale_Discount_Icon_70%25.png"
     },
 
@@ -23,7 +23,7 @@ var slide1Obj = [{
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy91bHRpbWF0ZS1mYWNld2FzaC1jb21iby01MTJ4NTEyLTEyODYxLnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJmaXQiOiJvdXRzaWRlIn19fQ==",
         name: "Beardo Ultimate Facewash Combo",
         cutPrice: "₹ 900",
-        price: "₹ 399",
+        price: "399",
         discountSticker: "https://cdn.beardo.in/vipclubsale2021/badge/VIP_Sale_Discount_Icon_56%25.png"
     },
     {
@@ -32,7 +32,7 @@ var slide1Obj = [{
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy9ncm93dGgtcHJvLWtpdC01MTJ4NTEyLTExOTg2LnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJmaXQiOiJvdXRzaWRlIn19fQ==",
         name: "Don Beardo's Beard Growth Pro Kit",
         cutPrice: "₹ 2095",
-        price: "₹ 888",
+        price: "888",
         discountSticker: "https://cdn.beardo.in/vipclubsale2021/badge/VIP_Sale_Discount_Icon_58%25.png"
     }
 
@@ -44,28 +44,28 @@ var topSellersProdObj = [{
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy9waG90by1mYWNlLWNvbWJvLTUxMng1MTItMzcxMzMucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjoxLCJoZWlnaHQiOjI4MywiZml0Ijoib3V0c2lkZSJ9fX0=",
         name: "Beardo Photo Face Combo",
         cutPrice: "₹ 1225",
-        price: "₹ 399",
+        price: "399",
         discountSticker: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidmlwY2x1YnNhbGUyMDIxL25ld2JhZGdlLzY3LnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJmaXQiOiJvdXRzaWRlIn19fQ=="
     },
     {
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy9oYWlyLWZhbGwtY29udHJvbC1raXQtNTEyeDUxMi0xMjEyMy5wbmciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjEsImhlaWdodCI6MjgzLCJmaXQiOiJvdXRzaWRlIn19fQ==",
         name: "Beardo Hair fall control kit (Shampoo, Serum & Growth oil)",
         cutPrice: "₹ 1395",
-        price: "₹ 699",
+        price: "699",
         discountSticker: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidmlwY2x1YnNhbGUyMDIxL25ld2JhZGdlLzU4LnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJmaXQiOiJvdXRzaWRlIn19fQ=="
     },
     {
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy9jaGFyY29hbC1kaXJ0eS1kdW8tY29tYm8tNTEyeDUxMi0zNzE2Ni5wbmciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjEsImhlaWdodCI6MjgzLCJmaXQiOiJvdXRzaWRlIn19fQ==",
         name: "Beardo Charcoal Dirty Duo combo",
         cutPrice: "₹ 625",
-        price: "₹ 299",
+        price: "299",
         discountSticker: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidmlwY2x1YnNhbGUyMDIxL25ld2JhZGdlLzUyLnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJmaXQiOiJvdXRzaWRlIn19fQ=="
     },
     {
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy93aGlza3ktc21va2UtcGVyZnVtZS01MTJ4NTEyLTM0OTMyLnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MSwiaGVpZ2h0IjoyODMsImZpdCI6Im91dHNpZGUifX19",
         name: "Beardo Whisky Smoke Perfume EDP (100ml)",
         cutPrice: "₹ 1200",
-        price: "₹ 499",
+        price: "499",
         discountSticker: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidmlwY2x1YnNhbGUyMDIxL25ld2JhZGdlLzU4LnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJmaXQiOiJvdXRzaWRlIn19fQ=="
     }
 ];
@@ -75,19 +75,19 @@ var topSellersProdObj = [{
 var hourlyPrizeObj = [{
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy9lbGl0ZS1iZWFyZG8tZ2lmdC1zZXQtNTEyeDUxMi0zNzAyMS5wbmciLCJlZGl0cyI6eyJyZXNpemUiOnsiZml0Ijoib3V0c2lkZSJ9fX0=",
         name: "Elite Beardo Gift Set",
-        price: "WORTH ₹3293",
+        price: "3293",
         winnerName: "@Rohan Gangurde"
     },
     {
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy91bHRpbWF0ZS1wZXJmdW1lLWNvbWJvLTUxMng1MTItMzcwMjUucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7ImZpdCI6Im91dHNpZGUifX19",
         name: "Ultimate Perfume combo",
-        price: "WORTH ₹3600",
+        price: "3600",
         winnerName: "@The MK 9 salon Razzaq kazi"
     },
     {
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy9hbWF6b24tY2FyZC1yczUwMDAtMTcweDIwMC0zNzAxNy5wbmciLCJlZGl0cyI6eyJyZXNpemUiOnsiZml0Ijoib3V0c2lkZSJ9fX0=",
         name: "Amazon Voucher worth ",
-        price: "WORTH ₹5000",
+        price: "5000",
         winnerName: "@Shivang Khanna"
     }
 
@@ -100,28 +100,28 @@ var hotSellersProdObj = [{
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy91bHRyYWdsb3ctZnctdWx0cmFnbG93LWxvdGlvbi01MTJ4NTEyLTM3MDkwLnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MSwiaGVpZ2h0IjoyODMsImZpdCI6Im91dHNpZGUifX19",
         name: "Beardo Ultraglow Face Cream & Ultraglow Facewash Combo",
         cutPrice: "₹ 675",
-        price: "₹ 299",
+        price: "299",
         discountSticker: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidmlwY2x1YnNhbGUyMDIxL25ld2JhZGdlLzU2LnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJmaXQiOiJvdXRzaWRlIn19fQ=="
     },
     {
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy9oYWlyLWZhbGwtY29udHJvbC1raXQtNTEyeDUxMi0xMjEyMy5wbmciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjEsImhlaWdodCI6MjgzLCJmaXQiOiJvdXRzaWRlIn19fQ==",
         name: "Beardo Hair fall control kit (Shampoo, Serum & Growth oil)",
         cutPrice: "₹ 1395",
-        price: "₹ 699",
+        price: "699",
         discountSticker: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidmlwY2x1YnNhbGUyMDIxL25ld2JhZGdlLzUwLnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJmaXQiOiJvdXRzaWRlIn19fQ=="
     },
     {
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy9hbGwtbmlnaHQtcGVyZnVtZS1kdW8tNTEyeDUxMi0xMzA1Ni5wbmciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjEsImhlaWdodCI6MjgzLCJmaXQiOiJvdXRzaWRlIn19fQ==",
         name: "Beardo All-Night Perfume Duo",
         cutPrice: "₹ 2700",
-        price: "₹ 799",
+        price: "799",
         discountSticker: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidmlwY2x1YnNhbGUyMDIxL25ld2JhZGdlLzcwLnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJmaXQiOiJvdXRzaWRlIn19fQ=="
     },
     {
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy91bHRpbWF0ZS1wZXJmdW1lLWNvbWJvLTUxMng1MTItMzUxODQucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjoxLCJoZWlnaHQiOjI4MywiZml0Ijoib3V0c2lkZSJ9fX0=",
         name: "Beardo Ultimate Perfume Combo",
         cutPrice: "₹ 3600",
-        price: "₹ 999",
+        price: "999",
         discountSticker: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidmlwY2x1YnNhbGUyMDIxL25ld2JhZGdlLzcyLnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJmaXQiOiJvdXRzaWRlIn19fQ=="
     }
 
@@ -136,56 +136,56 @@ var faceEssentialsProdObj = [{
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy9hY3RpdmF0ZWQtY2hhcmNvYWwtZmFjZXdhc2gtNTEyeDUxMi0zLTEzMDQ0LnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MSwiaGVpZ2h0IjoyODMsImZpdCI6Im91dHNpZGUifX19",
         name: "Beardo Activated Charcoal Facewash (100ml)",
         cutPrice: "₹ 275",
-        price: "₹ 175",
+        price: "175",
         discountSticker: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidmlwY2x1YnNhbGUyMDIxL25ld2JhZGdlLzM2LnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJmaXQiOiJvdXRzaWRlIn19fQ=="
     },
     {
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy9kZXRhbi1mYWNlLXNjcnViLTUxMng1MTItMzU0NzAucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjoxLCJoZWlnaHQiOjI4MywiZml0Ijoib3V0c2lkZSJ9fX0=",
         name: "Beardo Activated Charcoal Face Scrub (100g)",
         cutPrice: "₹ 350",
-        price: "₹ 199",
+        price: "199",
         discountSticker: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidmlwY2x1YnNhbGUyMDIxL25ld2JhZGdlLzQzLnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJmaXQiOiJvdXRzaWRlIn19fQ=="
     },
     {
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy9hY3RpdmF0ZWQtY2hhcmNvYWwtcGVlbC1vZmYtbWFzay01MTJ4NTEyLTEyODE4LnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MSwiaGVpZ2h0IjoyODMsImZpdCI6Im91dHNpZGUifX19",
         name: "Beardo Activated Charcoal Peel Off Mask (100g)",
         cutPrice: "₹ 350",
-        price: "₹ 199",
+        price: "199",
         discountSticker: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidmlwY2x1YnNhbGUyMDIxL25ld2JhZGdlLzQzLnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJmaXQiOiJvdXRzaWRlIn19fQ=="
     },
     {
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy9kZS10YW4tZmFjZXdhc2gtNTEyeDUxMi0xMjA0Ny5wbmciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjEsImhlaWdodCI6MjgzLCJmaXQiOiJvdXRzaWRlIn19fQ==",
         name: "Beardo De-Tan Facewash for Men (100ml)",
         cutPrice: "₹ 275",
-        price: "₹ 175",
+        price: "175",
         discountSticker: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidmlwY2x1YnNhbGUyMDIxL25ld2JhZGdlLzM2LnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJmaXQiOiJvdXRzaWRlIn19fQ=="
     },
     {
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy9kZS10YW4tcGVlbC1vZmYtbWFzay01MTJ4NTEyLTEyMjUxLnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MSwiaGVpZ2h0IjoyODMsImZpdCI6Im91dHNpZGUifX19",
         name: "Beardo De-Tan Peel Off Mask (100g)",
         cutPrice: "₹ 399",
-        price: "₹ 199",
+        price: "199",
         discountSticker: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidmlwY2x1YnNhbGUyMDIxL25ld2JhZGdlLzUwLnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJmaXQiOiJvdXRzaWRlIn19fQ=="
     },
     {
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy9kZXRhbi1mYWNlLXNjcnViLTUxMng1MTItMzU0NTcucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjoxLCJoZWlnaHQiOjI4MywiZml0Ijoib3V0c2lkZSJ9fX0=",
         name: "Beardo De-Tan Face Scrub (100g)",
         cutPrice: "₹ 350",
-        price: "₹ 199",
+        price: "199",
         discountSticker: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidmlwY2x1YnNhbGUyMDIxL25ld2JhZGdlLzQzLnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJmaXQiOiJvdXRzaWRlIn19fQ=="
     },
     {
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy84MzBkNWQ3Ny0xNzM0LTQxN2EtODk0Yi0xMTRlOWMxYjVhOWQtMzU4MDgucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjoxLCJoZWlnaHQiOjI4MywiZml0Ijoib3V0c2lkZSJ9fX0=",
         name: "Beardo HEMP Facewash Scrub (100ml)",
         cutPrice: "₹ 449",
-        price: "₹ 249",
+        price: "249",
         discountSticker: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidmlwY2x1YnNhbGUyMDIxL25ld2JhZGdlLzQ1LnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJmaXQiOiJvdXRzaWRlIn19fQ=="
     },
     {
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy9oZW1wLWZvYW0tZmFjZXdhc2gtNTEyeDUxMi0xMzMxMy5wbmciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjEsImhlaWdodCI6MjgzLCJmaXQiOiJvdXRzaWRlIn19fQ==",
         name: "Beardo HEMP Foam Facewash (100ml)",
         cutPrice: "₹ 399",
-        price: "₹ 199",
+        price: "199",
         discountSticker: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidmlwY2x1YnNhbGUyMDIxL25ld2JhZGdlLzUwLnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJmaXQiOiJvdXRzaWRlIn19fQ=="
     }
 ];
@@ -198,70 +198,70 @@ var mostWantedCombosProdObj = [{
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy9hY3RpdmF0ZWQtY2hhcmNvYWwtY29tYm8tZ3JleS1iYWNrZ3JvdW5kLTM1OTQ1LmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MSwiaGVpZ2h0IjoyODMsImZpdCI6Im91dHNpZGUifX19",
         name: "Beardo Activated Charcoal Combo",
         cutPrice: "₹ 875",
-        price: "₹ 499",
+        price: "499",
         discountSticker: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoibWFmaWEyMS9iYWRnZS80My5wbmciLCJlZGl0cyI6eyJyZXNpemUiOnsiZml0Ijoib3V0c2lkZSJ9fX0="
     },
     {
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy9nZXQtaGVtcHktY29tYm8tZ3JleS1iYWNrZ3JvdW5kMTEtMzYzODAuanBnIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjoxLCJoZWlnaHQiOjI4MywiZml0Ijoib3V0c2lkZSJ9fX0=",
         name: "Beardo Get Hempy Combo",
         cutPrice: "₹ 1093",
-        price: "₹ 349",
+        price: "349",
         discountSticker: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoibWFmaWEyMS9iYWRnZS82OC5wbmciLCJlZGl0cyI6eyJyZXNpemUiOnsiZml0Ijoib3V0c2lkZSJ9fX0="
     },
     {
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy8wMmluc3RhLWJyaWdodC1mYWNlLWNvbWJvLWdyZXktYmFja2dyb3VuZC0zNjkwMy5qcGciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjEsImhlaWdodCI6MjgzLCJmaXQiOiJvdXRzaWRlIn19fQ==",
         name: "Beardo Insta-bright Face Combo",
         cutPrice: "₹ 950",
-        price: "₹ 499",
+        price: "499",
         discountSticker: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoibWFmaWEyMS9iYWRnZS80Ny5wbmciLCJlZGl0cyI6eyJyZXNpemUiOnsiZml0Ijoib3V0c2lkZSJ9fX0="
     },
     {
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy92aXAtZGFya3NpZGUtZ2lmdC1zZXQtZ3JleS1iYWNrZ3JvdW5kLTEzMjExLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MSwiaGVpZ2h0IjoyODMsImZpdCI6Im91dHNpZGUifX19",
         name: "Beardo VIP DARKSIDE Gift",
         cutPrice: "₹ 2700",
-        price: "₹ 949",
+        price: "949",
         discountSticker: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoibWFmaWEyMS9iYWRnZS82NS5wbmciLCJlZGl0cyI6eyJyZXNpemUiOnsiZml0Ijoib3V0c2lkZSJ9fX0="
     },
     {
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy9nb2RmYXRoZXItZnJhZ3JhbmNlLWNvbWJvLWdyZXktYmFja2dyb3VuZC0xMjU3MC5qcGciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjEsImhlaWdodCI6MjgzLCJmaXQiOiJvdXRzaWRlIn19fQ==",
         name: "Beardo Godfather Fragrance Combo",
         cutPrice: "₹ 1798",
-        price: "₹ 799",
+        price: "799",
         discountSticker: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoibWFmaWEyMS9iYWRnZS81Ni5wbmciLCJlZGl0cyI6eyJyZXNpemUiOnsiZml0Ijoib3V0c2lkZSJ9fX0="
     },
     {
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy9jb2xvci1jYXJlLWNvbWJvLWdyZXktYmFja2dyb3VuZC0xMjcxMS5qcGciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjEsImhlaWdodCI6MjgzLCJmaXQiOiJvdXRzaWRlIn19fQ==",
         name: "Beardo Color & Care Combo",
         cutPrice: "₹ 1199",
-        price: "₹ 499",
+        price: "499",
         discountSticker: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoibWFmaWEyMS9iYWRnZS81OC5wbmciLCJlZGl0cyI6eyJyZXNpemUiOnsiZml0Ijoib3V0c2lkZSJ9fX0="
     },
     {
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy9oYWlyLWJlYXJkLXN0eWxpbmctZHVvLWNvbWJvLWdyZXktYmFja2dyb3VuZC0xMjU4MS5qcGciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjEsImhlaWdodCI6MjgzLCJmaXQiOiJvdXRzaWRlIn19fQ==",
         name: "Beardo Hair & Beard Styling Duo Combo",
         cutPrice: "₹ 750",
-        price: "₹ 399",
+        price: "399",
         discountSticker: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoibWFmaWEyMS9iYWRnZS80Ny5wbmciLCJlZGl0cyI6eyJyZXNpemUiOnsiZml0Ijoib3V0c2lkZSJ9fX0="
     },
     {
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy90aGUtcGVyZmVjdC1oYWlyLWNvbWJvLWdyZXktYmFja2dyb3VuZC0xMzI2OC5qcGciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjEsImhlaWdodCI6MjgzLCJmaXQiOiJvdXRzaWRlIn19fQ==",
         name: "Beardo The Perfect Hair Combo",
         cutPrice: "₹ 695",
-        price: "₹ 349",
+        price: "349",
         discountSticker: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoibWFmaWEyMS9iYWRnZS81MC5wbmciLCJlZGl0cyI6eyJyZXNpemUiOnsiZml0Ijoib3V0c2lkZSJ9fX0="
     },
     {
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy9kYXJrbmlnaHQtZnJhZ3JhbmNlLWNvbWJvLWdyZXktYmFja2dyb3VuZC0xMzA3NS5qcGciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjEsImhlaWdodCI6MjgzLCJmaXQiOiJvdXRzaWRlIn19fQ==",
         name: "Beardo DarkNight Fragrance Combo",
         cutPrice: "₹ 3299",
-        price: "₹ 999",
+        price: "999",
         discountSticker: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoibWFmaWEyMS9iYWRnZS83MC5wbmciLCJlZGl0cyI6eyJyZXNpemUiOnsiZml0Ijoib3V0c2lkZSJ9fX0="
     },
     {
         img_url: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoidXBsb2Fkcy9wb3dlci1ibGFjay1jb21iby1ncmV5LWJhY2tncm91bmQtMTMxOTkuanBnIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjoxLCJoZWlnaHQiOjI4MywiZml0Ijoib3V0c2lkZSJ9fX0=",
         name: "Beardo Power Black Combo",
         cutPrice: "₹ 999",
-        price: "₹ 599",
+        price: "599",
         discountSticker: "https://images.beardo.in/eyJidWNrZXQiOiJiZWFyZG9jIiwia2V5IjoibWFmaWEyMS9iYWRnZS81NS5wbmciLCJlZGl0cyI6eyJyZXNpemUiOnsiZml0Ijoib3V0c2lkZSJ9fX0="
     }
 ];
@@ -363,9 +363,9 @@ function showProd(prod, loc) {
         cp.innerText = el.cutPrice;
         // console.log(rupee);
         // let rupee = el.price[0];
-        let price = el.price.slice(2);
+        let price = el.price;
         // console.log((+price));
-        mp.innerText = price;
+        mp.innerText = +price;
 
         btn.innerText = "ADD TO CART";
 
